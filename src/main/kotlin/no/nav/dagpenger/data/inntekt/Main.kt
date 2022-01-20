77f0a0734f61461430eec133592d1ff6a5e1bb70
André Roaldseth <andre.roaldseth@nav.no>
2022-01-20 15:32:57 +0100

    Autentiser mot Schema Registry med basic auth

--- src/main/kotlin/no/nav/dagpenger/data/inntekt/Main.kt
+++ src/main/kotlin/no/nav/dagpenger/data/inntekt/Main.kt
@@ -13,10 +13,17 @@ import java.util.Properties
 
 private val aivenKafka: AivenConfig = AivenConfig.default
 private val avroProducerConfig = Properties().apply {
+    val schemaRegistryUser =
+        requireNotNull(System.getenv("KAFKA_SCHEMA_REGISTRY_USER")) { "Expected KAFKA_SCHEMA_REGISTRY_USER" }
+    val schemaRegistryPassword =
+        requireNotNull(System.getenv("KAFKA_SCHEMA_REGISTRY_PASSWORD")) { "Expected KAFKA_SCHEMA_REGISTRY_PASSWORD" }
+
     put(
         KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG,
         requireNotNull(System.getenv("KAFKA_SCHEMA_REGISTRY")) { "Expected KAFKA_SCHEMA_REGISTRY" }
     )
+    put(KafkaAvroSerializerConfig.USER_INFO_CONFIG, "$schemaRegistryUser:$schemaRegistryPassword")
+    put(KafkaAvroSerializerConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO")
     put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
     put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer::class.java)
 }
