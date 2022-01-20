831ad57c7dee22f8451675adcf5ff56765fabb50
André Roaldseth <andre.roaldseth@nav.no>
2022-01-20 20:59:01 +0100

    Lag en dings som produserer falske data

--- src/main/kotlin/no/nav/dagpenger/data/inntekt/Main.kt
+++ src/main/kotlin/no/nav/dagpenger/data/inntekt/Main.kt
@@ -3,13 +3,18 @@ package no.nav.dagpenger.data.inntekt
 import io.confluent.kafka.serializers.KafkaAvroSerializer
 import io.confluent.kafka.serializers.KafkaAvroSerializerConfig
 import io.netty.util.internal.ThreadExecutorMap.apply
+import mu.KotlinLogging
 import no.nav.dagpenger.data.inntekt.grunnbeløp.GGrunnbeløp
 import no.nav.helse.rapids_rivers.RapidApplication
+import no.nav.helse.rapids_rivers.RapidsConnection
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.StringSerializer
 import java.time.Duration
+import java.time.LocalDate
 import java.util.Properties
+import kotlin.concurrent.fixedRateTimer
 
 private val aivenKafka: AivenConfig = AivenConfig.default
 private val avroProducerConfig = Properties().apply {
@@ -27,16 +32,36 @@ private val avroProducerConfig = Properties().apply {
     put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
     put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer::class.java)
 }
+private val logger = KotlinLogging.logger { }
 
 fun main() {
     val env = System.getenv()
     val dagpengegrunnlagProducer by lazy {
         createProducer<String, Dagpengegrunnlag>(aivenKafka.producerConfig(avroProducerConfig))
     }
+    val fake = fixedRateTimer(name = "foo", period = 5000) {
+        Dagpengegrunnlag.newBuilder().apply {
+            beregningsdato = LocalDate.now()
+            gjeldendeGrunnbeloep = 123.0
+            grunnlag = listOf(
+                Grunnlag(Grunnlagsperiode.Siste12mnd, 345000.0),
+                Grunnlag(Grunnlagsperiode.Siste36mnd, 656000.0)
+            )
+            kontekst = Kontekst.Automatisering
+        }.build().also { grunnlag ->
+            logger.info { "Sender ut $grunnlag" }
+
+            dagpengegrunnlagProducer.send(ProducerRecord("teamdagpenger.data-inntekt-v1", grunnlag))
+        }
+    }
 
     RapidApplication.create(env) { _, rapidsConnection ->
         rapidsConnection.seekToBeginning()
-
+        rapidsConnection.register(object : RapidsConnection.StatusListener {
+            override fun onShutdown(rapidsConnection: RapidsConnection) {
+                fake.cancel()
+            }
+        })
         InntektRiver(rapidsConnection, dagpengegrunnlagProducer, GGrunnbeløp(timeToLive = Duration.ofHours(4)))
     }.start()
 }
