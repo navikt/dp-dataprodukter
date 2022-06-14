596f9a223d9a1928b5ece771923a7cb07a235658
André Roaldseth <andre.roaldseth@nav.no>
2022-06-14 09:33:35 +0200

    Bytt fra local-timezone-millis til timezone-millis som blir mappet til riktig BigQuery-type av connector

--- src/main/kotlin/no/nav/dagpenger/data/innlop/InnlopRiver.kt
+++ src/main/kotlin/no/nav/dagpenger/data/innlop/InnlopRiver.kt
@@ -6,6 +6,7 @@ import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
 import no.nav.helse.rapids_rivers.asLocalDateTime
+import java.time.ZoneId
 
 private val logger = KotlinLogging.logger { }
 
@@ -42,11 +43,15 @@ internal class InnlopRiver(
         }.register(this)
     }
 
+    companion object {
+        private val oslo: ZoneId = ZoneId.of("Europe/Oslo")
+    }
+
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
         Soknadsinnlop.newBuilder().apply {
             id = packet["@id"].asText()
-            opprettetDato = packet["@opprettet"].asLocalDateTime()
-            registrertDato = packet["datoRegistrert"].asLocalDateTime()
+            opprettetDato = packet["@opprettet"].asLocalDateTime().atZone(oslo).toInstant()
+            registrertDato = packet["datoRegistrert"].asLocalDateTime().atZone(oslo).toInstant()
             journalpostId = packet["journalpostId"].asText()
             skjemaKode = packet["skjemaKode"].asText()
             tittel = packet["tittel"].asText()
