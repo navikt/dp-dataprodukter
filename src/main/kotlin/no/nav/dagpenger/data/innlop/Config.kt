992e1d880124c5ba8061ee16a89b7e6194a12330
André Roaldseth <andre.roaldseth@nav.no>
2023-05-23 14:46:40 +0200

    Wrap config med propertygroup

--- src/main/kotlin/no/nav/dagpenger/data/innlop/Config.kt
+++ src/main/kotlin/no/nav/dagpenger/data/innlop/Config.kt
@@ -2,6 +2,7 @@ package no.nav.dagpenger.data.innlop
 
 import com.natpryce.konfig.ConfigurationProperties
 import com.natpryce.konfig.EnvironmentVariables
+import com.natpryce.konfig.PropertyGroup
 import com.natpryce.konfig.getValue
 import com.natpryce.konfig.overriding
 import com.natpryce.konfig.stringType
@@ -16,10 +17,12 @@ val kafka_produkt_soknad_tilstand_topic by stringType
 val kafka_produkt_soknad_ident_topic by stringType
 val kafka_produkt_soknad_dokumentkrav_topic by stringType
 
-val pdl_endpoint by stringType
-val pdl_scope by stringType
+object pdl : PropertyGroup() {
+    val pdl_endpoint by stringType
+    val pdl_scope by stringType
+}
 
-object azure {
+object azure : PropertyGroup() {
     val app_client_id by stringType
     val app_client_secret by stringType
     val app_config_token_endpoint by stringType
