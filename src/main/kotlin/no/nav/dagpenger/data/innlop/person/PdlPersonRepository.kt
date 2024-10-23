c25b8cf331c6d0c2295186ff16329960c08c00cb
André Roaldseth <andre.roaldseth@nav.no>
2024-10-23 10:26:00 +0200

    Legg til behandlingsnummer for PDL

--- src/main/kotlin/no/nav/dagpenger/data/innlop/person/PdlPersonRepository.kt
+++ src/main/kotlin/no/nav/dagpenger/data/innlop/person/PdlPersonRepository.kt
@@ -2,35 +2,42 @@ package no.nav.dagpenger.data.innlop.person
 
 import com.expediagroup.graphql.client.spring.GraphQLWebClient
 import com.expediagroup.graphql.client.types.GraphQLClientError
+import io.ktor.http.HttpHeaders
 import kotlinx.coroutines.runBlocking
 import no.nav.pdl.HentPerson
 import no.nav.pdl.enums.AdressebeskyttelseGradering
+import org.slf4j.MDC
 
 class PdlPersonRepository(
     private val client: GraphQLWebClient,
     private val tokenExchange: () -> String,
 ) : PersonRepository {
     private companion object {
-        private val beskyttet = listOf(
-            AdressebeskyttelseGradering.STRENGT_FORTROLIG,
-            AdressebeskyttelseGradering.STRENGT_FORTROLIG_UTLAND,
-        )
+        private val beskyttet =
+            listOf(
+                AdressebeskyttelseGradering.STRENGT_FORTROLIG,
+                AdressebeskyttelseGradering.STRENGT_FORTROLIG_UTLAND,
+            )
     }
 
     override fun hentPerson(ident: String): PersonRepository.Person {
-        val person = runBlocking {
-            client.execute(HentPerson(HentPerson.Variables(ident))) {
-                header("Authorization", "Bearer ${tokenExchange()}")
-            }.also {
-                if (it.errors?.isNotEmpty() == true) {
-                    throw RuntimeException(
-                        "Kall mot PDL feilet. Feil: ${
-                            it.errors!!.joinToString("\n", transform = GraphQLClientError::toString)
-                        }",
-                    )
-                }
-            }
-        }.data!!.hentPerson
+        val person =
+            runBlocking {
+                client
+                    .execute(HentPerson(HentPerson.Variables(ident))) {
+                        header(HttpHeaders.Authorization, "Bearer ${tokenExchange()}")
+                        header(HttpHeaders.XRequestId, MDC.get("journalpostId"))
+                        header("behandlingsnummer", "B342")
+                    }.also {
+                        if (it.errors?.isNotEmpty() == true) {
+                            throw RuntimeException(
+                                "Kall mot PDL feilet. Feil: ${
+                                    it.errors!!.joinToString("\n", transform = GraphQLClientError::toString)
+                                }",
+                            )
+                        }
+                    }
+            }.data!!.hentPerson
 
         return PersonRepository.Person(
             harAdressebeskyttelse = person!!.adressebeskyttelse.any { it.gradering in beskyttet },
