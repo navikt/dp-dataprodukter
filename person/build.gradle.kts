e6199dd83570a842e09715970195597d77c74af7
André Roaldseth <andre.roaldseth@nav.no>
2024-11-01 15:46:37 +0100

    Flytt junit4 exlusion til mockk

--- person/build.gradle.kts
+++ person/build.gradle.kts
@@ -13,11 +13,10 @@ dependencies {
     implementation("com.expediagroup", "graphql-kotlin-spring-client", "8.2.1")
     implementation("com.nimbusds:oauth2-oidc-sdk:11.20.1")
 
-    testImplementation("no.nav.security:mock-oauth2-server:2.1.10") {
+    testImplementation("no.nav.security:mock-oauth2-server:2.1.10")
+    testImplementation(libs.mockk) {
         exclude(group = "junit", module = "junit")
     }
-
-    testImplementation(libs.mockk)
     testImplementation("org.wiremock:wiremock:3.9.2")
     testImplementation(libs.bundles.jackson)
 }
