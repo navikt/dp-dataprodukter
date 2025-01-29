2b1d8366f00b6116c45ddfce227acd005db45d80
Cabrol <maria.cabrol@nav.no>
2025-01-29 09:52:23 +0100

    slutte å populere dataprodukt med ident og journalpostId

--- src/main/kotlin/no/nav/dagpenger/dataprodukter/produkter/innlop/SoknadsinnlopRiver.kt
+++ src/main/kotlin/no/nav/dagpenger/dataprodukter/produkter/innlop/SoknadsinnlopRiver.kt
@@ -9,7 +9,6 @@ import com.github.navikt.tbd_libs.rapids_and_rivers_api.RapidsConnection
 import io.micrometer.core.instrument.MeterRegistry
 import mu.KotlinLogging
 import mu.withLoggingContext
-import no.nav.dagpenger.dataprodukt.innlop.Ident
 import no.nav.dagpenger.dataprodukt.innlop.Soknadsinnlop
 import no.nav.dagpenger.dataprodukter.asUUID
 import no.nav.dagpenger.dataprodukter.avro.asTimestamp
@@ -19,7 +18,6 @@ import no.nav.dagpenger.dataprodukter.person.PersonRepository
 internal class SoknadsinnlopRiver(
     rapidsConnection: RapidsConnection,
     private val dataTopic: DataTopic<Soknadsinnlop>,
-    private val identTopic: DataTopic<Ident>,
     private val personRepository: PersonRepository,
 ) : River.PacketListener {
     init {
@@ -93,16 +91,5 @@ internal class SoknadsinnlopRiver(
         }
 
         if (person.harAdressebeskyttelse) return
-
-        Ident
-            .newBuilder()
-            .apply {
-                this.journalpostId = journalpostId
-                this.ident = ident
-            }.build()
-            .also {
-                identTopic.publiser(it)
-                logger.info { "Publiserer rad for ${it::class.java.simpleName}" }
-            }
     }
 }
