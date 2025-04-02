8d1fe343adb5b273a586c54d51f5b4e7ba52e075
Maria Cabrol <53761746+mariacabrol@users.noreply.github.com>
2025-04-02 09:14:44 +0200

    fjerne topic for ident ved søknad som ikke benyttes (#142)

--- src/main/kotlin/no/nav/dagpenger/dataprodukter/Main.kt
+++ src/main/kotlin/no/nav/dagpenger/dataprodukter/Main.kt
@@ -4,14 +4,12 @@ import no.nav.dagpenger.dataprodukt.behandling.Behandling
 import no.nav.dagpenger.dataprodukt.innlop.Soknadsinnlop
 import no.nav.dagpenger.dataprodukt.soknad.Dokumentkrav
 import no.nav.dagpenger.dataprodukt.soknad.SoknadFaktum
-import no.nav.dagpenger.dataprodukt.soknad.SoknadIdent
 import no.nav.dagpenger.dataprodukt.soknad.SoknadTilstand
 import no.nav.dagpenger.dataprodukter.kafka.DataTopic.Companion.dataTopic
 import no.nav.dagpenger.dataprodukter.person.PdlPersonRepository
 import no.nav.dagpenger.dataprodukter.produkter.behandling.VedtakRiver
 import no.nav.dagpenger.dataprodukter.produkter.innlop.SoknadsinnlopRiver
 import no.nav.dagpenger.dataprodukter.produkter.søknad.DokumentkravRiver
-import no.nav.dagpenger.dataprodukter.produkter.søknad.SøknadIdentRiver
 import no.nav.dagpenger.dataprodukter.produkter.søknad.SøknadInnsendtRiver
 import no.nav.dagpenger.dataprodukter.produkter.søknad.SøknadTilstandRiver
 import no.nav.dagpenger.dataprodukter.produkter.søknad.SøknadsdataRiver
@@ -22,7 +20,6 @@ internal object DataTopics {
     val soknadsinnlop = dataTopic<Soknadsinnlop>(config[kafka_produkt_topic])
     val soknadFaktum = dataTopic<SoknadFaktum>(config[kafka_produkt_soknad_faktum_topic])
     val soknadTilstand = dataTopic<SoknadTilstand>(config[kafka_produkt_soknad_tilstand_topic])
-    val soknadIdent = dataTopic<SoknadIdent>(config[kafka_produkt_soknad_ident_topic])
     val dokumentkrav = dataTopic<Dokumentkrav>(config[kafka_produkt_soknad_dokumentkrav_topic])
     val behandlingTopic = dataTopic<Behandling>(config[kafka_produkt_behandling_topic])
 }
@@ -42,7 +39,6 @@ fun main() {
             SøknadsdataRiver(rapidsConnection, søknadRepository, personRepository)
             SøknadInnsendtRiver(rapidsConnection, søknadRepository, DataTopics.soknadFaktum)
             SøknadTilstandRiver(rapidsConnection, DataTopics.soknadTilstand, personRepository)
-            SøknadIdentRiver(rapidsConnection, DataTopics.soknadIdent, personRepository)
             DokumentkravRiver(rapidsConnection, DataTopics.dokumentkrav, personRepository)
 
             // Behandling
