0c88eea6d11b43d0cf327edce8b06204c2f89a02
André Roaldseth <andre.roaldseth@nav.no>
2025-04-11 08:40:33 +0200

    Bytt å bruke OpenAPI spec for å hente ut data

---
+++ behandling/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("common")
+    `java-library`
+    id("org.openapi.generator") version "7.12.0"
+}
+
+dependencies {
+    implementation(libs.bundles.jackson)
+}
+
+sourceSets {
+    main {
+        kotlin {
+            srcDir("build/generated/src/main/kotlin")
+        }
+    }
+}
+
+tasks {
+    compileKotlin {
+        dependsOn("openApiGenerate")
+    }
+}
+
+openApiGenerate {
+    generatorName.set("kotlin")
+    remoteInputSpec.set(
+        "https://raw.githubusercontent.com/navikt/dp-behandling/refs/heads/main/openapi/src/main/resources/behandling-api.yaml",
+    )
+    outputDir.set("${layout.buildDirectory.get()}/generated/")
+    packageName.set("no.nav.dagpenger.behandling.api")
+    globalProperties.set(mapOf("models" to ""))
+    modelNameSuffix.set("DTO")
+    typeMappings = mapOf("DateTime" to "LocalDateTime")
+    importMappings = mapOf("LocalDateTime" to "java.time.LocalDateTime")
+    configOptions.set(
+        mapOf(
+            "serializationLibrary" to "jackson",
+            "dateLibrary" to "custom",
+            "enumPropertyNaming" to "original",
+        ),
+    )
+}
