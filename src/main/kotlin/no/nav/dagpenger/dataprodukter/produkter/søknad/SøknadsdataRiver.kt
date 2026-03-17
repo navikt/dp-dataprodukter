3710bb1883c82f6bc9533dd4e54f7843773c2d6e
ArselanSultani <arselan.sultani@nav.no>
2026-03-17 14:37:26 +0100

    Test hardkodet seksjonssvar

--- src/main/kotlin/no/nav/dagpenger/dataprodukter/produkter/søknad/SøknadsdataRiver.kt
+++ src/main/kotlin/no/nav/dagpenger/dataprodukter/produkter/søknad/SøknadsdataRiver.kt
@@ -213,7 +213,7 @@ internal class OrkestratorSøknadsdataRiver(
             val seksjonJson = objectMapper.readTree(seksjon)
             Seksjonsinfo.newBuilder()
                 .setSeksjonId(seksjonJson["seksjonId"].asText())
-                .setSeksjonsvar(seksjonJson["seksjonsvar"].asText())
+                .setSeksjonsvar("""{"test": 123}""")
                 .setVersjon(seksjonJson["versjon"].asText())
                 .build()
         } catch (e: Exception) {
