5499c18b7ffe509650b0f7e8de86206c503d673c
ArselanSultani <arselan.sultani@nav.no>
2026-03-18 15:50:15 +0100

    Revert "Teste å kunne sende json direkte til BigQuery istedenfor å sende bruke Avro"
    This reverts commit 31557e73b9350a73ea368bf7fe78a44063e47ac5.

--- src/main/kotlin/no/nav/dagpenger/dataprodukter/kafka/AivenConfig.kt
+++ src/main/kotlin/no/nav/dagpenger/dataprodukter/kafka/AivenConfig.kt
@@ -86,11 +86,4 @@ class AivenConfig(
             put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer::class.java)
             putAll(producerConfig(properties))
         }
-
-    fun jsonProducerConfig(properties: Properties = Properties()) =
-        Properties().apply {
-            put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
-            put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
-            putAll(producerConfig(properties))
-        }
 }
