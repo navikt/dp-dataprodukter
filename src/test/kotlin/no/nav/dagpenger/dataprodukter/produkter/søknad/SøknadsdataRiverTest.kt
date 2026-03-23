250e7535b1fcca130e0055b9f2481792db89edd9
ArselanSultani <arselan.sultani@nav.no>
2026-03-23 13:09:33 +0100

    Send hver seksjon seperat til BQ (#238)

--- src/test/kotlin/no/nav/dagpenger/dataprodukter/produkter/søknad/SøknadsdataRiverTest.kt
+++ src/test/kotlin/no/nav/dagpenger/dataprodukter/produkter/søknad/SøknadsdataRiverTest.kt
@@ -5,7 +5,6 @@ import com.github.navikt.tbd_libs.rapids_and_rivers.test_support.TestRapid
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
-import no.nav.dagpenger.dataprodukt.soknad.OrkestratorSoknad
 import no.nav.dagpenger.dataprodukt.soknad.SoknadFaktum
 import no.nav.dagpenger.dataprodukter.helpers.Seksjoner
 import no.nav.dagpenger.dataprodukter.helpers.faktum
@@ -20,6 +19,12 @@ import org.apache.kafka.clients.producer.KafkaProducer
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import java.util.UUID
+import kotlin.test.assertTrue
+import kotlin.text.contains
+import kotlin.text.startsWith
+import no.nav.dagpenger.dataprodukt.soknad.OrkestratorSeksjon
+import no.nav.dagpenger.dataprodukt.soknad.OrkestratorSoknad
+import org.apache.kafka.clients.producer.ProducerRecord
 
 internal class SøknadsdataRiverTest {
     private val repository = InMemorySøknadRepository()
@@ -93,11 +98,13 @@ private fun getSøknadData(søknadId: UUID) =
     }
 
 internal class OrkestratorSøknadsdataRiverTest {
-    private val producer = mockk<KafkaProducer<String, OrkestratorSoknad>>(relaxed = true)
-    private val dataTopic = DataTopic(producer, "orkestrator-søknadsdata")
+    private val seksjonProducer = mockk<KafkaProducer<String, OrkestratorSeksjon>>(relaxed = true)
+    private val søknadProducer = mockk<KafkaProducer<String, OrkestratorSoknad>>(relaxed = true)
+    private val seksjonTopic = DataTopic(seksjonProducer, "orkestrator-seksjon")
+    private val søknadTopic = DataTopic(søknadProducer, "orkestrator-søknad")
     private val rapid =
         TestRapid().also {
-            OrkestratorSøknadsdataRiver(it, dataTopic)
+            OrkestratorSøknadsdataRiver(it, seksjonTopic, søknadTopic)
         }
 
     @AfterEach
@@ -108,11 +115,75 @@ internal class OrkestratorSøknadsdataRiverTest {
     @Test
     fun `Mottar søknadsdata fra orkestrator og publiserer til dataTopic`() {
         val søknadId = UUID.randomUUID()
+        val søknadSlot = mutableListOf<ProducerRecord<String, OrkestratorSoknad>>()
+        val seksjonSlot = mutableListOf<ProducerRecord<String, OrkestratorSeksjon>>()
+        every { seksjonProducer.send(capture(seksjonSlot), any()) } returns mockk()
+        every { søknadProducer.send(capture(søknadSlot), any()) } returns mockk()
+
         rapid.sendTestMessage(getOrkestratorSøknadEvent(søknadId))
 
-        verify(exactly = 1) {
-            producer.send(any(), any())
+        verify(exactly = 10) {
+            seksjonProducer.send(any(), any())
         }
+        val capturedRecordForSeksjon = seksjonSlot.first()
+        val personalia = capturedRecordForSeksjon.value()
+        assert(personalia != null)
+        assertTrue(personalia["seksjonId"] == "personalia")
+        assertTrue(personalia.seksjonsvar["folkeregistrertAdresseErNorgeStemmerDet"] == "ja")
+
+        val dinSituasjon = seksjonSlot[1].value()
+        assert(dinSituasjon != null)
+        assertTrue(dinSituasjon["seksjonId"] == "din-situasjon")
+        assertTrue (dinSituasjon.seksjonsvar["harDuMottattDagpengerFraNavILøpetAvDeSiste52Ukene"] == "vetikke")
+
+        val arbeidsforhold = seksjonSlot[2].value()
+        assert(arbeidsforhold != null)
+        assertTrue(arbeidsforhold["seksjonId"] == "arbeidsforhold")
+        assertTrue(arbeidsforhold.seksjonsvar["hvordanHarDuJobbet"] == "harIkkeJobbetDeSiste36Månedene")
+
+        val annenPengestotte = seksjonSlot[3].value()
+        assert(annenPengestotte != null)
+        assertTrue(annenPengestotte["seksjonId"] == "annen-pengestotte")
+        assertTrue(annenPengestotte.seksjonsvar["harMottattEllerSøktOmPengestøtteFraAndreEøsLand"] == "nei")
+
+        val egenNaring = seksjonSlot[4].value()
+        assert(egenNaring != null)
+        assertTrue(egenNaring["seksjonId"] == "egen-naring")
+        assertTrue(egenNaring.seksjonsvar["driverDuEgenNæringsvirksomhet"] == "nei")
+
+        val verneplikt = seksjonSlot[5].value()
+        assert(verneplikt != null)
+        assertTrue(verneplikt["seksjonId"] == "verneplikt")
+        assertTrue(verneplikt.seksjonsvar["avtjentVerneplikt"] == "nei")
+
+        val utdanning = seksjonSlot[6].value()
+        assert(utdanning != null)
+        assertTrue(utdanning["seksjonId"] == "utdanning")
+        assertTrue(utdanning.seksjonsvar["tarUtdanningEllerOpplæring"] == "ja")
+
+        val barnetillegg = seksjonSlot[7].value()
+        assert(barnetillegg != null)
+        assertTrue(barnetillegg["seksjonId"] == "barnetillegg")
+        assertTrue(barnetillegg.seksjonsvar["forsørgerDuBarnSomIkkeVisesHer"] == "nei")
+
+        val reellArbeidssoker = seksjonSlot[8].value()
+        assert(reellArbeidssoker != null)
+        assertTrue(reellArbeidssoker["seksjonId"] == "reell-arbeidssoker")
+        assertTrue(reellArbeidssoker.seksjonsvar["kanDuJobbeBådeHeltidOgDeltid"] == "ja")
+
+        val tilleggsopplysninger = seksjonSlot[9].value()
+        assert(tilleggsopplysninger != null)
+        assertTrue(tilleggsopplysninger["seksjonId"] == "tilleggsopplysninger")
+        assertTrue(tilleggsopplysninger.seksjonsvar["harTilleggsopplysninger"] == "nei")
+
+        verify(exactly = 1) {
+            søknadProducer.send(any(), any())
+        }
+        val capturedRecordForSøknad = søknadSlot.first()
+        val søknad = capturedRecordForSøknad.value()
+        assert(søknad != null)
+        assertTrue(søknad.soknadId.toString() == søknadId.toString())
+
     }
 
     @Test
@@ -121,7 +192,11 @@ internal class OrkestratorSøknadsdataRiverTest {
         rapid.sendTestMessage(getSøknadEndretTilstandUtenKilde(søknadId))
 
         verify(exactly = 0) {
-            producer.send(any(), any())
+            seksjonProducer.send(any(), any())
+        }
+
+        verify(exactly = 0) {
+            søknadProducer.send(any(), any())
         }
     }
 
@@ -131,8 +206,22 @@ internal class OrkestratorSøknadsdataRiverTest {
         rapid.sendTestMessage(getOrkestratorSøknadEvent(søknadId, gjeldendeTilstand = "Påbegynt"))
 
         verify(exactly = 0) {
-            producer.send(any(), any())
+            seksjonProducer.send(any(), any())
         }
+    }
+
+    @Test
+    fun `Arbeidsforhold i seksjonsvar er serialisert riktig som JSON string`() {
+        val søknadId = UUID.randomUUID()
+        val slot = mutableListOf<ProducerRecord<String, OrkestratorSeksjon>>()
+        every { seksjonProducer.send(capture(slot), any()) } returns mockk()
+
+        rapid.sendTestMessage(getOrkestratorSøknadEventWithNestedArbeidsforhold(søknadId))
+
+        verify(exactly = 2) {
+            seksjonProducer.send(any(), any())
+        }
+
     }
 }
 
@@ -144,16 +233,56 @@ private fun getOrkestratorSøknadEvent(
     val søknadsdata = mapOf(
         "opprettet" to "2024-01-01T12:00:00",
         "innsendt" to "2024-01-01T12:30:00",
-        "personalia" to """{"seksjonId":"personalia","seksjonsvar":{"fornavnFraPdl":"FIRKANTET","etternavnFraPdl":"JEGER","alderFraPdl":"54","poststedFraPdl":"Dilling","landkodeFraPdl":"NO","landFraPdl":"NORGE","kontonummerFraKontoregister":"","folkeregistrertAdresseErNorgeStemmerDet":"ja"},"versjon":1}""",
-        "din-situasjon" to """{"seksjonId":"din-situasjon","seksjonsvar":{"harDuMottattDagpengerFraNavILøpetAvDeSiste52Ukene":"vetikke","hvilkenDatoSøkerDuDagpengerFra":"2026-03-03"},"versjon":1}""",
-        "arbeidsforhold" to """{"seksjonId":"arbeidsforhold","seksjonsvar":{"hvordanHarDuJobbet":"harIkkeJobbetDeSiste36Månedene","registrerteArbeidsforhold":[]},"versjon":1}""",
-        "annen-pengestotte" to """{"seksjonId":"annen-pengestotte","seksjonsvar":{"harMottattEllerSøktOmPengestøtteFraAndreEøsLand":"nei","pengestøtteFraAndreEøsLand":[],"mottarDuAndreUtbetalingerEllerØkonomiskeGoderFraTidligereArbeidsgiver":"nei","pengestøtteFraTidligereArbeidsgiver":[],"mottarDuPengestøtteFraAndreEnnNav":"nei","pengestøtteFraNorge":[]},"versjon":1}""",
-        "egen-naring" to """{"seksjonId":"egen-naring","seksjonsvar":{"driverDuEgenNæringsvirksomhet":"nei","næringsvirksomheter":null,"driverDuEgetGårdsbruk":"nei","gårdsbruk":null},"versjon":1}""",
-        "verneplikt" to """{"seksjonId":"verneplikt","seksjonsvar":{"avtjentVerneplikt":"nei","dokumentasjonskrav":"null"},"versjon":1}""",
-        "utdanning" to """{"seksjonId":"utdanning","seksjonsvar":{"tarUtdanningEllerOpplæring":"ja","dokumentasjonskrav":"null"},"versjon":1}""",
-        "barnetillegg" to """{"seksjonId":"barnetillegg","versjon":1,"seksjonsvar":{"barnFraPdl":null,"forsørgerDuBarnSomIkkeVisesHer":"nei","barnLagtManuelt":null}}""",
-        "reell-arbeidssoker" to """{"seksjonId":"reell-arbeidssoker","seksjonsvar":{"kanDuJobbeBådeHeltidOgDeltid":"ja","kanDuJobbeIHeleNorge":"ja","kanDuTaAlleTyperArbeid":"ja","erDuVilligTilÅBytteYrkeEllerGåNedILønn":"ja","dokumentasjonskrav":"null"},"versjon":1}""",
-        "tilleggsopplysninger" to """{"seksjonId":"tilleggsopplysninger","seksjonsvar":{"harTilleggsopplysninger":"nei"},"versjon":1}""",
+        "personalia" to mapOf(
+            "seksjonsdata" to """{"seksjonId":"personalia","seksjonsvar":{"fornavnFraPdl":"FIRKANTET","etternavnFraPdl":"JEGER","alderFraPdl":"54","poststedFraPdl":"Dilling","landkodeFraPdl":"NO","landFraPdl":"NORGE","kontonummerFraKontoregister":"","folkeregistrertAdresseErNorgeStemmerDet":"ja"},"versjon":1}""",
+            "opprettet" to "2024-01-01T12:00:00",
+            "oppdatert" to "2024-01-01T12:00:00",
+        ),
+        "din-situasjon" to mapOf(
+            "seksjonsdata" to """{"seksjonId":"din-situasjon","seksjonsvar":{"harDuMottattDagpengerFraNavILøpetAvDeSiste52Ukene":"vetikke","hvilkenDatoSøkerDuDagpengerFra":"2026-03-03"},"versjon":1}""",
+            "opprettet" to "2024-01-01T12:00:00",
+            "oppdatert" to "2024-01-01T12:00:00",
+        ),
+        "arbeidsforhold" to mapOf(
+            "seksjonsdata" to """{"seksjonId":"arbeidsforhold","seksjonsvar":{"hvordanHarDuJobbet":"harIkkeJobbetDeSiste36Månedene","registrerteArbeidsforhold":[]},"versjon":1}""",
+            "opprettet" to "2024-01-01T12:00:00",
+            "oppdatert" to "2024-01-01T12:00:00",
+        ),
+        "annen-pengestotte" to mapOf(
+            "seksjonsdata" to """{"seksjonId":"annen-pengestotte","seksjonsvar":{"harMottattEllerSøktOmPengestøtteFraAndreEøsLand":"nei","pengestøtteFraAndreEøsLand":[],"mottarDuAndreUtbetalingerEllerØkonomiskeGoderFraTidligereArbeidsgiver":"nei","pengestøtteFraTidligereArbeidsgiver":[],"mottarDuPengestøtteFraAndreEnnNav":"nei","pengestøtteFraNorge":[]},"versjon":1}""",
+            "opprettet" to "2024-01-01T12:00:00",
+            "oppdatert" to "2024-01-01T12:00:00",
+        ),
+        "egen-naring" to mapOf(
+            "seksjonsdata" to """{"seksjonId":"egen-naring","seksjonsvar":{"driverDuEgenNæringsvirksomhet":"nei","næringsvirksomheter":null,"driverDuEgetGårdsbruk":"nei","gårdsbruk":null},"versjon":1}""",
+            "opprettet" to "2024-01-01T12:00:00",
+            "oppdatert" to "2024-01-01T12:00:00",
+        ),
+        "verneplikt" to mapOf(
+            "seksjonsdata" to """{"seksjonId":"verneplikt","seksjonsvar":{"avtjentVerneplikt":"nei","dokumentasjonskrav":"null"},"versjon":1}""",
+            "opprettet" to "2024-01-01T12:00:00",
+            "oppdatert" to "2024-01-01T12:00:00",
+        ),
+        "utdanning" to mapOf(
+            "seksjonsdata" to """{"seksjonId":"utdanning","seksjonsvar":{"tarUtdanningEllerOpplæring":"ja","dokumentasjonskrav":"null"},"versjon":1}""",
+            "opprettet" to "2024-01-01T12:00:00",
+            "oppdatert" to "2024-01-01T12:00:00",
+        ),
+        "barnetillegg" to mapOf(
+            "seksjonsdata" to """{"seksjonId":"barnetillegg","versjon":1,"seksjonsvar":{"barnFraPdl":null,"forsørgerDuBarnSomIkkeVisesHer":"nei","barnLagtManuelt":null}}""",
+            "opprettet" to "2024-01-01T12:00:00",
+            "oppdatert" to "2024-01-01T12:00:00",
+        ),
+        "reell-arbeidssoker" to mapOf(
+            "seksjonsdata" to """{"seksjonId":"reell-arbeidssoker","seksjonsvar":{"kanDuJobbeBådeHeltidOgDeltid":"ja","kanDuJobbeIHeleNorge":"ja","kanDuTaAlleTyperArbeid":"ja","erDuVilligTilÅBytteYrkeEllerGåNedILønn":"ja","dokumentasjonskrav":"null"},"versjon":1}""",
+            "opprettet" to "2024-01-01T12:00:00",
+            "oppdatert" to "2024-01-01T12:00:00",
+        ),
+        "tilleggsopplysninger" to mapOf(
+            "seksjonsdata" to """{"seksjonId":"tilleggsopplysninger","seksjonsvar":{"harTilleggsopplysninger":"nei"},"versjon":1}""",
+            "opprettet" to "2024-01-01T12:00:00",
+            "oppdatert" to "2024-01-01T12:00:00",
+        ),
     )
 
     return JsonMessage
@@ -181,6 +310,37 @@ private fun getSøknadEndretTilstandUtenKilde(søknadId: UUID) =
                 "gjeldendeTilstand" to "Innsendt",
             ),
         ).toJson()
+
+private fun getOrkestratorSøknadEventWithNestedArbeidsforhold(søknadId: UUID): String {
+    val søknadsdata = mapOf(
+        "opprettet" to "2024-01-01T12:00:00",
+        "innsendt" to "2024-01-01T12:30:00",
+        "personalia" to mapOf(
+            "seksjonsdata" to """{"seksjonId":"personalia","seksjonsvar":{"fornavnFraPdl":"FIRKANTET"},"versjon":"1"}""",
+            "opprettet" to "2024-01-01T12:00:00",
+            "oppdatert" to "2024-01-01T12:00:00",
+        ),
+        "arbeidsforhold" to mapOf(
+            "seksjonsdata" to """{"seksjonId":"arbeidsforhold","seksjonsvar":{"hvordanHarDuJobbet":"fastArbeidstidIMindreEnn6Måneder","harDuJobbetIEtAnnetEøsLandSveitsEllerStorbritanniaILøpetAvDeSiste36Månedene":"nei","registrerteArbeidsforhold":[{"navnetPåBedriften":"Jobb AS","hvilketLandJobbetDuI":"NOR","hvordanHarDetteArbeidsforholdetEndretSeg":"jegErPermitert","id":"d368ee1c-427d-4885-ada7-7aa792e4a52a"}]},"versjon":"1"}""",
+            "opprettet" to "2024-01-01T12:00:00",
+            "oppdatert" to "2024-01-01T12:00:00",
+        )
+    )
+
+    return JsonMessage
+        .newMessage(
+            "søknad_endret_tilstand",
+            mapOf(
+                "søknad_uuid" to søknadId.toString(),
+                "ident" to "12345678901",
+                "forrigeTilstand" to "Påbegynt",
+                "gjeldendeTilstand" to "Innsendt",
+                "kilde" to "orkestrator",
+                "@opprettet" to "2024-01-01T12:00:00",
+                "søknadsdata" to søknadsdata,
+            ),
+        ).toJson()
+}
 
 private fun getDataMessage(
     uuid: UUID,
