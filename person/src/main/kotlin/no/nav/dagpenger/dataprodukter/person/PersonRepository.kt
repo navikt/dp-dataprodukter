35757688c98f376862bec7c2f3bd3a989b3c0744
ArselanSultani <arselan.sultani@nav.no>
2026-03-27 14:37:59 +0100

    Refaktorere litt klassenavn

--- person/src/main/kotlin/no/nav/dagpenger/dataprodukter/person/PersonRepository.kt
+++ person/src/main/kotlin/no/nav/dagpenger/dataprodukter/person/PersonRepository.kt
@@ -1,6 +1,6 @@
 package no.nav.dagpenger.dataprodukter.person
 
 interface PersonRepository {
-    fun hentPerson(ident: String): Person
-    fun hentPersonMedKode6OgKode7Beskyttelse(ident: String): Person
+    fun hentPerson(ident: String): PersonsBeskyttelseInfo
+    fun hentPersonMedKode6Og7BeskyttelseInfo(ident: String): PersonsBeskyttelseInfo
 }
